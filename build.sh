8e81f068e9032090102f7b3fecddd82cae29c460
Enodevs <isiaqabdullah100@gmail.com>
2025-02-02 14:11:40 +0100

    Update collection of static files

--- build.sh
+++ build.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
-
-# Check Python version
-python --version
-
-# Upgrade pip
-python -m pip install --upgrade pip
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run migrations
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
